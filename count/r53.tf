1f3080cfa443e57c0171fa0515c319cfff949f88
Jayani22 <mutcherlajayani@gmail.com>
2025-11-04 17:43:17 +0530

    Terraform-count

--- count/r53.tf
+++ count/r53.tf
@@ -1,15 +1,4 @@
 resource "aws_route53_record" "roboshop" {
-    count = 10
-    zone_id = "${var.zone_id}"
-    name = "${var.instances[count.index]}.${var.domain_name}"
-    type = "A"
-    ttl = 1
-    records = [aws_instance.terraform[count.index].private_ip]
-    allow_overwrite = true
-}
-
-resource "aws_route53_record" "frontend" {
-    count = 1
     zone_id = "${var.zone_id}"
     name = "${var.instances[count.index]}.${var.domain_name}"
     type = "A"
