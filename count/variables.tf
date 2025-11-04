a4359ec3277ff973d630b9a1b10e3fd98e864420
Jayani22 <mutcherlajayani@gmail.com>
2025-11-04 08:18:20 +0530

    terraform-count

--- count/variables.tf
+++ count/variables.tf
@@ -1,5 +1,5 @@
 variable "instances" {
-    default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+    default = [ "mongodb", "redis", "mysql", "rabbitmq", "catalogue", "cart", "user", "shipping", "payment", "frontend"]
 }
 
 variable "zone_id" {
