aa0ac9aa39ac54c81a67443c84a6e263b83775ef
Jayani22 <mutcherlajayani@gmail.com>
2025-11-04 07:20:20 +0530

    terraform-count

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+}
+
+variable "zone_id" {
+    default = "Z08769191SZ0EERXMPHL9"
+}
+
+variable "domain_name" { 
+    default = "jayani23.fun"
+}
